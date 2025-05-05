320b16ef5ad8806ef7ced1f74c3c64cb3ff43872
Yuliia <yuliadzyoba@gmail.com>
2025-05-05 14:43:30 +0300

    add MainLayout

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import './App.css';
+import React, { Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+const MainLayout = React.lazy(() => import('../pages/MainLayout/MainLayout'));
+const HomePage = React.lazy(() => import('../pages/HomePage/HomePage'));
+
+const NotFoundPage = React.lazy(() =>
+  import('../pages/NotFoundPage/NotFoundPage')
+);
 
 function App() {
   return (
     <div>
-      <h2>App</h2>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<MainLayout />} />
+          <Route path="/home" element={<HomePage />} />
+
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 }
---
+++ src/pages/MainLayout/MainLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './MainLayout.module.css';
+
+const MainLayout = () => {
+  return (
+    <div>
+      <h2>MainLayout</h2>
+    </div>
+  );
+};
+
+export default MainLayout;
