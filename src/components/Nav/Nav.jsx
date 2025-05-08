76c9cf9aa404ede901a32098b8225248b8e4bd14
Yuliia <yuliadzyoba@gmail.com>
2025-05-09 01:27:10 +0300

    add Nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import styles from './Nav.module.css';
 
 const Nav = () => {
   return (
-    <div>
-      <h2>Nav</h2>
-    </div>
+    <nav className={styles.nav}>
+      <NavLink
+        to="/news"
+        className={({ isActive }) => (isActive ? styles.active : '')}
+      >
+        News
+      </NavLink>
+      <NavLink
+        to="/notices"
+        className={({ isActive }) => (isActive ? styles.active : '')}
+      >
+        Notices
+      </NavLink>
+      <NavLink
+        to="/friends"
+        className={({ isActive }) => (isActive ? styles.active : '')}
+      >
+        Our Friends
+      </NavLink>
+    </nav>
   );
 };
 
