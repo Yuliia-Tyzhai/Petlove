fcb99e5f4a80a027c279774f5f8b4a3a79a50834
Yuliia <yuliadzyoba@gmail.com>
2025-05-13 14:23:48 +0300

    add NewsPage

--- src/pages/NewsPage/NewsPage.jsx
+++ src/pages/NewsPage/NewsPage.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Title from '../../components/Title/Title';
+import SearchField from '../components/SearchField/SearchField';
+import NewsList from '../components/NewsList/NewsList';
+import Pagination from '../components/Pagination/Pagination';
+import { fetchNews } from '../services/newsApi';
+import styles from './NewsPage.module.css';
 
 const NewsPage = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [news, setNews] = useState([]);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    fetchNews(searchQuery, page, setNews);
+  }, [searchQuery, page]);
+
   return (
-    <div>
-      <h2>NewsPage</h2>
+    <div className={styles.newsPage}>
+      <Title />
+      <SearchField onSearch={setSearchQuery} />
+      <NewsList news={news} />
+      <Pagination currentPage={page} setPage={setPage} />
     </div>
   );
 };
